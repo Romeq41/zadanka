e76af6b6f26d28945e4782fde22807cdf14353fc
Romeq41 <jakubromanowski41@gmail.com>
2022-10-04 00:27:25 +0200

    commit nr2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+using namespace std;
 
+int Policz(int a, int b){
+    return a + b;
+}
 int main() {
-    std::cout << "Hello, World!123123" << std::endl;
+    cout << "Hello, World!123123" << endl;
+    cout << "Pierwszy branch; 2 + 3 = "  <<Policz(2,3) << endl;
+
     return 0;
 }
