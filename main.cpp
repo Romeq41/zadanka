dd0d07cd10f0c4bea503408348128c780eedd3e6
Romeq41 <jakubromanowski41@gmail.com>
2022-10-04 00:35:15 +0200

    commit nr1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+<<<<<<< HEAD
 int Policz (int a, int b) {
-    return a+b;
+    return (a+b)*(a-b);
 }
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout <<"W pierwszym branchu; 2+3=" << Policz(2,3) << std::endl;
+=======
+using namespace std;
+
+
+int main() {
+    cout << "Hello, World!123123" << endl;
+
+
+>>>>>>> 6b3e3b4 (commit nr1)
     return 0;
 }
