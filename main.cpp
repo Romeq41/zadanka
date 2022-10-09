07be02eb7cf0f74d17f2bde330d6e505ee57e19f
Romeq41 <jakubromanowski41@gmail.com>
2022-10-09 14:02:30 +0200

    Zmiany dodatkowe w 03

--- main.cpp
+++ main.cpp
@@ -1,19 +1,8 @@
 #include <iostream>
-<<<<<<< HEAD
-int Policz (int a, int b) {
-    return (a+b)*(a-b);
-}
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout <<"W pierwszym branchu; 2+3=" << Policz(2,3) << std::endl;
-=======
 using namespace std;
-
 
 int main() {
     cout << "Hello, World!123123" << endl;
-
-
->>>>>>> 6b3e3b4 (commit nr1)
+    
     return 0;
 }
